141070d0e52aab3d229770bc09179e901fad627f
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-02-16 13:58:19 +0100

    Refactored Angular code in client

---
+++ RawStack/app/myApp.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+    var module = angular.module("myApp", []);
+
+    module.controller("moviesCtrl", function ($scope, $http) {
+        $http.get("/api/movies").then(function (e) {
+            $scope.movies = e.data;
+        });
+        
+        $scope.newMovie = { Title: "" };
+        $scope.addMovie = function () {
+            $http.post("/api/movies", $scope.newMovie).then(function () {
+                window.location = window.location;
+            });
+        };
+    });
+}());
