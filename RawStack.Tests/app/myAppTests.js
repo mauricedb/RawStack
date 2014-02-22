09f944ff33aae44e859e6b66ff61b4d51a15302d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-02-22 19:09:03 +0000

    Added unit tests for the movies controller

---
+++ RawStack.Tests/app/myAppTests.js
@@ -0,0 +1,37 @@
+﻿describe("The moviesCtrl", function () {
+    'use strict';
+
+    beforeEach(module("myApp"));
+
+    it("can be created", inject(function ($controller) {
+        var scope = {};
+        var ctrl = $controller("moviesCtrl", {
+            $scope: scope
+        });
+
+        expect(ctrl).toBeDefined();
+    }));
+});
+
+describe("The moviesCtrl scope", function () {
+    'use strict';
+
+    var scope;
+
+    beforeEach(module("myApp"));
+
+    beforeEach(inject(function ($controller) {
+        scope = {};
+        $controller("moviesCtrl", {
+            $scope: scope
+        });
+    }));
+
+    it("has a newMovie object", function () {
+        expect(scope.newMovie).toEqual({ Title: "" });
+    });
+
+    it("has a addMovie function", function () {
+        expect(typeof scope.addMovie).toBe("function");
+    });
+});
