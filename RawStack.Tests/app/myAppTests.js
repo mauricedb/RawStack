a0656f37f7f8ba5faaccdaa9891112a4f872af12
Maurice de Beijer <maurice.de.beijer@gmail.com>
2014-02-28 19:46:02 +0100

    Add HTTP request unit tests for the movies controller

--- RawStack.Tests/app/myAppTests.js
+++ RawStack.Tests/app/myAppTests.js
@@ -35,3 +35,42 @@ describe("The moviesCtrl scope", function () {
         expect(typeof scope.addMovie).toBe("function");
     });
 });
+
+
+describe("The moviesCtrl http requests", function () {
+    'use strict';
+
+    var scope, httpBackend;
+
+    beforeEach(module("myApp"));
+
+    beforeEach(inject(function ($controller, $httpBackend) {
+        httpBackend = $httpBackend;
+
+        scope = {};
+        $controller("moviesCtrl", {
+            $scope: scope
+        });
+    }));
+
+    it("should populate the movies array when the HTTP request succeeds", function () {
+        var movies = [
+            { id: 1, title: "A movie" },
+            { id: 2, title: "Another movie" }];
+
+        httpBackend.expect("GET", "/api/movies").respond(movies);
+
+        httpBackend.flush();
+
+        expect(scope.movies).toEqual(movies);
+    });
+
+    it("should not populate the movies array when the HTTP request fails", function () {
+
+        httpBackend.expect("GET", "/api/movies").respond(404);
+
+        httpBackend.flush();
+
+        expect(scope.movies).toBeUndefined();
+    });
+});
