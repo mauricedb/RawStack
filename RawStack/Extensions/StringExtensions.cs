c58f73479259e296b891f52053c6ebe8ae41ea2a
Maurice de Beijer <mauricedb@computer.org>
2014-10-22 21:58:19 +0200

    Added EditorFor to razor views to generate AngularJS markup

---
+++ RawStack/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class StringExtensions
+    {
+        // Convert the string to camel case.
+        public static string ToCamelCase(this string value)
+        {
+            return value.Substring(0, 1).ToLowerInvariant() + value.Substring(1);
+        }
+    }
+}
--- RawStack/Models/Movie.cs
+++ RawStack/Models/Movie.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls.WebParts;
 
 namespace RawStack.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public int? Year { get; set; }
+        [UIHint("Int32")]
         public string MpaaRating { get; set; }
         public Rating Ratings { get; set; }
+        [DisplayName("Critics Consensus"), UIHint("TextArea")]
         public string CriticsConsensus { get; set; }
+        [UIHint("TextArea")]
         public string Synopsis { get; set; }
         public IEnumerable<string> Genres { get; set; }
         public Posters Posters { get; set; }
